9916164272e97ae07d5c1c8abd56e972af9f2efb
Michael Wilson <info@michaelwilsion.io>
2018-05-28 15:13:31 -0400

    Some initialization changes

--- main.js
+++ main.js
@@ -4,8 +4,14 @@ const { app, BrowserWindow } = require( 'electron' );
 
 function createWindow() {
     win = new BrowserWindow({
-        width: 800,
-        height: 600
+        width: 900,
+        minWidth: 900,
+        height: 605,
+        minHeight: 605,
+        show: true,
+        fullscreen: false,
+        maximizable: true,
+        resizable: true
     });
 
     win.loadFile( 'index.html' );
@@ -18,8 +24,9 @@ function createWindow() {
 
 }
 
-app.on( 'ready', createWindow );
 
+
+app.on( 'ready', createWindow );
 app.on( 'activate', () => {
     if ( win === null ) {
         createWindow();
